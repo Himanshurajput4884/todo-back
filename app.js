0a5138ad79ecbda398d9abca53eff1be58141fe3
Himanshu <himanshurajput6654@gmail.com>
2023-07-24 17:01:45 +0530

    deploying

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app=express()
 const port=5001
 const taskRoutes = require('./routes/tasks') 
 const connection = require("./db/connection");
+const cookieParser = require("cookie-parser");
 require('dotenv').config()
 
 var cors = require('cors');
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(express.json())
-
+app.use(cookieParser());
 app.use(taskRoutes);
 
 app.listen(port, ()=>{
