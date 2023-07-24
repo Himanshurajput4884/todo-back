6e1eb5299e8bd25ef7c423bb8969e5cda1d9409a
Himanshu <himanshurajput6654@gmail.com>
2023-07-24 18:31:17 +0530

    adding frontend url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 var cors = require('cors');
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000", "https://todo-front-rho.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
